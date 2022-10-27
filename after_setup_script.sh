943766ba27531d28c9f54e4cf7697619547f3b58
Oliver-Tautz <53008918+Oliver-Tautz@users.noreply.github.com>
2022-10-27 13:18:10 +0200

    Update after_setup_script.sh

--- after_setup_script.sh
+++ after_setup_script.sh
@@ -169,7 +169,7 @@ fi
 
 # add some lines to Localsettings (activate extensions)
 cp $LOCALSETTINGS_PATH $LOCALSETTINGS_PATH_TMP
-echo $ADD_TO_LOCALSETTINGS >> $LOCALSETTINGS_PATH_TMP
+echo "$ADD_TO_LOCALSETTINGS" >> $LOCALSETTINGS_PATH_TMP
 
 # copy LocalSettings.php to volume
 docker cp $LOCALSETTINGS_PATH_TMP "$MEDIAWIKI_CONTAINER_NAME:/var/www/html/LocalSettings.php"
